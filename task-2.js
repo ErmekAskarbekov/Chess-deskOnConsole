ae787278340cca6eaaa5bf67e073feb21cc120bd
Ermek Askarbekov <ermaaskarbekov@gmail.com>
2022-10-14 11:47:22 +0600

    I created an HTML file and added a JS file to it
    Launched compiler from TS to JS

---
+++ task-2.js
@@ -0,0 +1,16 @@
+"use strict";
+let string = '';
+let size = 8;
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        if ((i + j) % 2 === 1) {
+            string += '██';
+        }
+        else {
+            string += '  ';
+        }
+    }
+    string += '\n';
+}
+;
+console.log(string);
